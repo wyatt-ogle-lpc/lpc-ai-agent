d7bcf4c03238a0750fb6ed8f0373d2bb514cb52d
Wyatt Ogle <wyatt.ogle@lloydpest.com>
2025-08-12 17:17:06 +0000

    updated two files for production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,4 @@
-# config/initializers/session_store.rb
-
-# This configuration is for the development environment.
-if Rails.env.development?
-    Rails.application.config.session_store :active_record_store,
-      key: '_lpc_agent_app_session',
-      same_site: :lax
-  end
+Rails.application.config.session_store :active_record_store,
+  key: "_lpc_ai_agent_session",
+  same_site: :lax,
+  secure: Rails.env.production?
